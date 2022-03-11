dbbced5ef4d601fa535152de1c1c6ff3c98dcc76
Iris-Smok <irissmok11@gmail.com>
2022-03-11 16:39:36 +0000

    Add more tests and code

--- student.py
+++ student.py
@@ -4,6 +4,7 @@ student.py
 
 from datetime import date, timedelta
 # to allow us to define start and end date
+import requests
 
 
 class Student:
@@ -37,3 +38,21 @@ class Student:
         alert santa
         """
         self.naughty_list = True
+
+    def apply_extension(self, days):
+        """
+        student end date
+        """
+        self.end_date = self.end_date + timedelta(days=days)
+
+    def course_schedule(self):
+        """
+        course schedule
+        """
+        response = requests.get(
+            f"https://company.com/course-schedule/{self._last_name}/{self._first_name}")
+
+        if response.ok:
+            return response.text
+        else:
+            return "Something went wrong"
--- test_student.py
+++ test_student.py
@@ -2,6 +2,8 @@
 test_student.py
 """
 import unittest
+from datetime import timedelta
+from unittest.mock import patch
 from student import Student
 
 
@@ -9,32 +11,68 @@ class TestStudent(unittest.TestCase):
     """
     Student class test
     """
+    @classmethod
+    def setUpClass(cls):
+        print('setUpClass')
+
+    @classmethod
+    def tearDownClass(cls):
+        print('tearDownClass')
+
+    def setUp(self):
+        print('setUp')
+        self.student = Student('John', 'Doe')
+
+    def tearDown(self):
+        print('tearDown')
 
     def test_full_name(self):
         """
         create an instance of the class in order
         to test it
         """
-        student = Student('John', 'Doe')
-        self.assertEqual(student.full_name, 'John Doe')
+        print('test_full_name')
+        self.assertEqual(self.student.full_name, 'John Doe')
         # checking if full_name method returns correct
 
     def test_email(self):
         """
         test email
         """
-        student = Student('John', 'Doe')
-        self.assertEqual(student.email, 'john.doe@email.com')
+        print('test_email')
+        self.assertEqual(self.student.email, 'john.doe@email.com')
 
     def test_alert_santa(self):
         """
         add student to Santa naughy list test
         """
-        student = Student('John', 'Doe')
-        student.alert_santa()
+        print('test_alert_santa')
+        self.student.alert_santa()
 
-        self.assertTrue(student.naughty_list)
+        self.assertTrue(self.student.naughty_list)
 
+    def test_apply_extension(self):
+        """
+        student end date
+        """
+        old_end_date = self.student.end_date
+        self.student.apply_extension(5)
+
+        self.assertEqual(self.student.end_date, old_end_date +
+                         timedelta(days=5))
+
+    def test_course_schedule_failed(self):
+        """
+        In the path "student" comes from the name of the file student.py
+        If you have named the file something else - it will need to match
+        eg, students.py would need "students.requests.get"
+        """
+        with patch("student.requests.get") as mocked_get:
+            mocked_get.return_value.ok = False
+
+            schedule = self.student.course_schedule()
+            self.assertEqual(schedule, "Something went wrong")
+            
 
 if __name__ == "__main__":
     unittest.main()
